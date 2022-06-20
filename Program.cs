9685e23dbede047b171242627a52d1c20ac95119
Andrew <andriycherpak.education@gmail.com>
2022-06-21 02:42:37 +0300

    Code documentation added

--- Program.cs
+++ Program.cs
@@ -4,13 +4,20 @@ using System.Windows.Forms;
 
 namespace Курсова
 {
-    static class Program
+    internal static class Program
     {
+        /// <summary>
+        /// зберігає масив елементів для використання по всій програмі
+        /// </summary>
         public static List<int> InputedArray = new List<int>();
+        
+        /// <summary>
+        /// Показує, закрили ми форму програмно, ачи це зробив сам користувач. Якщо користувач - інші форма теж має бути закрито. Інакше обробити це як програмне закриття і продовжити роботу минулих/наступних форм
+        /// </summary>
         public static bool IsClosedByUser = true;
 
         /// <summary>
-        ///  The main entry point for the application.
+        ///  Вхідна точка програми
         /// </summary>
         [STAThread]
         static void Main()
